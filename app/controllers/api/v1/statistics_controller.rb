3adb7486456088ff84d371e8b14b066ef2df8e9d
Xavier <xavier@flowlens.com>
2017-07-17 17:25:33 +0200

    serializer statistics

--- app/controllers/api/v1/statistics_controller.rb
+++ app/controllers/api/v1/statistics_controller.rb
@@ -1,8 +1,15 @@
 class Api::V1::StatisticsController < Api::V1::ApiController
 
   def index
-    @statistics = Statistic.all
-    expose @statistics
+    @statistics = Statistic.all.joins(:player).includes(:player)
+
+    @statistics = @statistics.where(season: params[:season]) if params[:season].present?
+    @statistics = @statistics.where("players.position = ? ", params[:position]) if params[:position].present?
+    @statistics = @statistics.where("players.active = ? ", params[:active]) if params[:active].present?
+
+    #@statistics = @statistics.pluck("week_#{params[:round]}") if params[:round].present?
+
+    expose @statistics, include: [:player]
   end
 
 end
