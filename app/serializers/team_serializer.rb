831720ac17d39204ec37b924e7e09a806444c2ce
Xavier <xavier@flowlens.com>
2017-08-01 17:27:47 +0200

    fixing team serializer

--- app/serializers/team_serializer.rb
+++ app/serializers/team_serializer.rb
@@ -7,7 +7,6 @@
 class TeamSerializer < EstadisticasSerializer
 
   has_many :players, serializer: PlayerSerializer
-  has_many :games, serializer: GameSerializer
 
   attributes :name, :short_code, :position, :played, :won, :lost, :rest_round_1, :rest_round_2, :playing_cup
 
