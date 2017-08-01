67a3eeb67cb66095b916ea97e31c5820f36a80ac
Xavier <xavier@flowlens.com>
2017-08-01 16:36:41 +0200

    wrong played_games serialized

--- app/serializers/player_serializer.rb
+++ app/serializers/player_serializer.rb
@@ -9,6 +9,6 @@ class PlayerSerializer < EstadisticasSerializer
   belongs_to :team, serializer: TeamSerializer
   has_many :statistics, serializer: StatisticSerializer
 
-  attributes :name, :position, :height, :weithg, :date_of_birth, :place_of_birth, :image, :number, :price
+  attributes :name, :position, :height, :weithg, :date_of_birth, :place_of_birth, :image, :number, :price, :played_games
 
 end
